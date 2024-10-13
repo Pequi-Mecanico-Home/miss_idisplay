10c681cf4b66b287c89a7a34d3d53c5234907630
BatCarlos <carlos.eduardo.ramos.duarte@gmail.com>
2024-10-13 01:41:16 -0300

    feature of video_feed and subtitle created

--- flaskServer/app.py
+++ flaskServer/app.py
@@ -1,85 +1,102 @@
-# This module sets up a Flask web server with SocketIO integration to handle real-time
-# websocket communication and HTTP requests.
-# Routes:
-# - /: Renders the index.html template.
-# - /html: Receives data from a form, logs the data, and sends an HTML file based on the data.
-# - /trigger_html_change: Emits a 'change_html' event via SocketIO.
-# - /post_trigger_html_change: Receives data from a POST request and emits an 'html_updated' event with the data.
-# SocketIO Events:
-# - my_event: Handles a custom event, increments a session counter, and emits a response.
-# - test_message: Handles 'test_message' events, prints the received message, and emits a test response.
-# - change_html: Handles 'change_html' events, prints a message, and broadcasts an 'html_updated' event.
-# Global Variables:
-# - app: The Flask application instance.
-# - socketio: The SocketIO instance for the Flask app.
-# - thread: A placeholder for a background thread (currently unused).
-# - thread_lock: A threading lock to manage access to the background thread.
+# Imports for OpenCV operations
+import cv2
+from cv_bridge import CvBridge
 
+# Imports for ROS2
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
 
-from threading import Lock
-from flask import Flask, render_template, session, send_file, request
-from flask_socketio import SocketIO, emit
-import os
-import logging
+# Imports for threading operations
+import signal, sys
+from threading import Thread, Event, Lock
+
+from flask import Flask, render_template, request, Response
+from flask_socketio import SocketIO
+
 
 async_mode = None
+
+frame = None # Global variable frame (the holy image)
+
+bridge = CvBridge()
+event = Event()
+
+def on_image(msg):
+    global frame
+    
+    cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding = "passthrough")
+    frame = cv2.imencode(".jpg",cv_image)[1].tobytes()
+    event.set()
+
+rclpy.init(args=None)
+node = rclpy.create_node('Show_image_python')
+
+Thread(target=lambda:node).start() # Starting the Thread with a target in the node
+
+subscription = node.create_subscription(Image,"/image", on_image, 10) # Creating the Subscribe node
 
 app = Flask(__name__)
 socketio = SocketIO(app, async_mode=async_mode)
 thread = None
 thread_lock = Lock()
 
+## Function that runs 'spin_once' and waits for a new frame
+def get_frame():
+    rclpy.spin_once(node,timeout_sec=1.0)
+    event.wait()
+    event.clear()
+    return frame
+
+## Function that prepare the frame to be loaded into template
+def gen():
+    while True:
+        frame = get_frame()
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+## Function that finish the actual context
+def signal_handler(signal, frame):
+    rclpy.shutdown()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT,signal_handler) # Calls the 'signal_handler' and finish the actual signal (like a Ctrl + C)
+
 # To set a endpoint in the web page, we use @app.route, each endpoint has an utility
 @app.route('/')
-# With this function the code render the html page choosed and set the async_mode to be useful with socketIO and its events
+# With this function I can render the html page I send and set the async_mode to be useful with socketIO and its events
 def index():
     return render_template('index.html', async_mode=socketio.async_mode)
 
-@app.route('/html')
-def get_html():
-    data = request.form.get('data')
-    # Configure logging
-    logging.basicConfig(level=logging.INFO)
-    logger = logging.getLogger(__name__)
-
-    logger.info(f'Received data: {data}')
-    if data:
-        html_path = f'/static/html-css/{data}.html' 
-        print("html path", html_path) 
-        return send_file(html_path, mimetype='text/html')
-    return "No data provided", 501
-    
-@app.route('/trigger_html_change')
-def trigger_html_change():
-    socketio.emit('change_html')
-    return "html change event emitted"
-
-@socketio.event
-def my_event(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']})
-
-@socketio.on('test_message')
-def handle_message(data):
-    print('received message: ' + str(data))
-    emit('test_response', {'data': 'Test response sent'})
-
-@socketio.on('change_html')
-def change_html():
-    print('html change requested')
-    emit('html_updated', {'data': 'html has been updated'}, broadcast=True)
-
 @app.route('/post_trigger_html_change', methods=['POST'])
 def post_trigger_html_change():
-# declares data and retrieves the value associated with form data
+# declares data and retrievesthe value associated with form data
     data = request.form.get('data')
 # After providing some data the socket.emit send tht html_updated to the index.html with the correct data
     if data:
         socketio.emit('html_updated', {'data': data})
         return "html change event emitted with data"
-# If no data is sent, returns an error
     return "No data provided", 400
-# Ensuring that the server starts only when the script is executed directly
+
+## Function that insert the new frame in template
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'file' not in request.files:
+        return 'No file part', 400
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file', 400
+    if file:
+        content = file.read().decode('utf-8')
+        for line in content.splitlines():
+            socketio.emit('subtitle', line)
+            socketio.sleep(1)  # Wait for 3 seconds before sending the next line
+    return 'File uploaded successfully \n', 200
+ 
 if __name__ == '__main__':
     socketio.run(app)
